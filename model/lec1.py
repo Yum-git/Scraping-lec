10ed5ce102366c6009b808650c5bab8ac063d75c
Yum-git <yzk.yuma@gmail.com>
2021-04-28 20:52:03 +0900

    lec1 lec2追加

---
+++ model/lec1.py
@@ -0,0 +1,89 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import os
+
+
+class Main(object):
+    # 初期設定
+    def __init__(self, base_url: str, url: str):
+        self.base_url = base_url
+        self.url = url
+        self.url_title = None
+        self.soup = None
+
+    # BeautifulSoupオブジェクトを生成
+    def soup_gene(self):
+        # webサイトのデータを取得する
+        response = requests.get(self.url)
+        response.encoding = response.apparent_encoding
+
+        # 取得したデータを扱えるデータにパースする
+        self.soup = BeautifulSoup(response.text, 'lxml')
+        self.soup_title()
+
+    # タイトルを取得する
+    def soup_title(self):
+        # ページのタイトルを取得する
+        self.url_title = self.soup.select_one('title').getText()
+        # 同じページ名なので特殊な書き方をする
+        # 普通はこんな書き方しなくてもいいです
+        self.url_title += self.soup.select_one('h1').getText()
+        # imgを保存する際のフォルダを生成する
+        self.dir_create()
+
+    # フォルダを作成する
+    def dir_create(self):
+        # フォルダが存在しない場合のみフォルダを生成する条件文
+        if os.path.exists('img') is False:
+            os.mkdir('img')
+        if os.path.exists('img/' + self.url_title) is False:
+            os.mkdir('img/' + self.url_title)
+
+    # ページ移動が行われた際に行う関数
+    def change_url(self, url: str):
+        # 探査urlを変える
+        self.url = url
+        # BeautifulSoupオブジェクトを生成する
+        self.soup_gene()
+
+    # 画像を取得する
+    def img_get(self):
+        print('画像を取得')
+        print()
+
+        idx = 0
+        # 画像を抽出する
+        for img_soup in self.soup.select('img'):
+            # 画像urlを抽出する
+            img_url = self.base_url + img_soup.get('src')
+            print(idx, '枚目', img_url)
+            # 画像をバイナリデータにて取得する
+            response = requests.get(img_url)
+            # 画像を保存する
+            with open("img/" + self.url_title + "/{}.jpg".format(idx), "wb") as f:
+                f.write(response.content)
+            idx += 1
+            # 取得間隔を調整する
+            time.sleep(1)
+
+    # 次のページに移動できる
+    def next_page(self):
+        print('次のページに移動する')
+        print()
+
+        # 全てのaタグを抽出する
+        for a_tag in self.soup.select('a'):
+            a_title = a_tag.getText()
+            if a_title == '次のページ':
+                a_url = a_tag.get('href')
+                self.change_url(self.base_url + a_url)
+                break
+
+    # タグを好きに変更できる関数
+    def practice_print(self):
+        print('練習用関数')
+        print()
+        # ↓ここを好きに書き換える↓
+        for tag_ in self.soup.select(''):
+            print(tag_)
---
+++ model/lec2.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+
+import base64
+import os
+
+
+class Main(object):
+    def __init__(self, url: str):
+        self.url = url
+        self.url_title = None
+        self.driver = None
+        self.soup = None
+
+        self.idx = 0
+
+    # webdriverの初期設定
+    def driver_init(self):
+        # どんな環境でもSeleniumで動かせるよー
+        op = Options()
+        op.add_argument("--disable-gpu")
+        op.add_argument("--disable-extensions")
+        op.add_argument("--proxy-server='direct://'")
+        op.add_argument("--proxy-bypass-list=*")
+        op.add_argument("--start-maximized")
+        op.add_argument(
+            "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36")
+        op.add_argument("--headless")
+        op.add_argument('--no-sandbox')
+        op.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome('chromedriver', options=op)
+
+    # ページ移動時の関数
+    def driver_wait(self):
+        selector = 'body'
+        self.driver.get(self.url)
+        WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, selector))
+        )
+
+    def canvas_get(self):
+        self.dir_create()
+        print('canvas内の画像を取得する')
+
+        canvas_first = self.driver.find_element_by_css_selector('#book > div.page.first.right > div.page_unit.page_image > canvas')
+        canvas_second = self.driver.find_element_by_css_selector('#book > div.page.second.left > div.page_unit.page_image > canvas')
+
+        base64_first = self.driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas_first)
+        base64_second = self.driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas_second)
+        first_png = base64.b64decode(base64_first)
+        second_png = base64.b64decode(base64_second)
+
+        with open("img/canvas_{}.png".format(self.idx), 'wb') as f:
+            f.write(first_png)
+        with open("img/canvas_{}.png".format(self.idx + 1), 'wb') as f:
+            f.write(second_png)
+
+    def nextpage_click(self):
+        print('次のページに移動する')
+        next_page = self.driver.find_element_by_css_selector('.flip.flip-left')
+        next_page.click()
+        self.idx += 2
+
+    # フォルダを作成する
+    def dir_create(self):
+        # フォルダが存在しない場合のみフォルダを生成する条件文
+        if os.path.exists('img') is False:
+            os.mkdir('img')
