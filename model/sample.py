b55ea3b54fbc58edafc2f88df91ffaca42ffe204
Yum-git <yzk.yuma@gmail.com>
2021-07-02 09:36:53 +0900

    Fix 関数説明文を修正

--- model/sample.py
+++ model/sample.py
@@ -10,6 +10,7 @@ import os
 
 class Main(object):
     def __init__(self, url: str):
+        """class初期設定"""
         self.url = url
         self.url_title = None
         self.driver = None
@@ -17,24 +18,21 @@ class Main(object):
 
         self.idx = 0
 
-    # webdriverの初期設定
     def driver_init(self):
-        # どんな環境でもSeleniumで動かせるよー
+        """webdriverの初期設定"""
         op = Options()
         op.add_argument("--disable-gpu")
         op.add_argument("--disable-extensions")
         op.add_argument("--proxy-server='direct://'")
         op.add_argument("--proxy-bypass-list=*")
         op.add_argument("--start-maximized")
-        op.add_argument(
-            "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36")
         op.add_argument("--headless")
         op.add_argument('--no-sandbox')
         op.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome('chromedriver', options=op)
 
-    # ページ移動時の関数
     def driver_wait(self):
+        """ページ移動時の関数"""
         selector = 'body'
         self.driver.get(self.url)
         WebDriverWait(self.driver, 30).until(
@@ -42,6 +40,7 @@ class Main(object):
         )
 
     def canvas_get(self):
+        """canvas内の画像を取得する関数"""
         self.dir_create()
         print('canvas内の画像を取得する')
 
@@ -56,13 +55,14 @@ class Main(object):
             f.write(first_png)
 
     def next_page_click(self):
+        """次のページに移動する関数"""
         print('次のページに移動する')
         next_page = self.driver.find_element_by_css_selector('#output')
         next_page.click()
         self.idx += 1
 
-    # フォルダを作成する
     def dir_create(self):
+        """保存先フォルダを作成する関数"""
         self.url_title = self.driver.title.split("-")[0].strip()
         # フォルダが存在しない場合のみフォルダを生成する条件文
         if os.path.exists('img') is False:
